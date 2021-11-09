723a5a90df0eec3cd2ee4fdbeddf0742e66af433
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-11-09 13:02:32 +0100

    Empty components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './components/administration/admin/admin.component';
+import { UserComponent } from './components/administration/user/user.component';
+import { AuthenticationComponent } from './components/administration/authentication/authentication.component';
+import { RegistrationComponent } from './components/administration/registration/registration.component';
+import { HeaderNavBarComponent } from './components/navigation/header-nav-bar/header-nav-bar.component';
+import { LeftNavBarComponent } from './components/navigation/left-nav-bar/left-nav-bar.component';
+import { GridComponent } from './components/product/grid/grid.component';
+import { DetailsPopupComponent } from './components/product/details-popup/details-popup.component';
+import { ComponentContainerComponent } from './components/component-container/component-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    UserComponent,
+    AuthenticationComponent,
+    RegistrationComponent,
+    HeaderNavBarComponent,
+    LeftNavBarComponent,
+    GridComponent,
+    DetailsPopupComponent,
+    ComponentContainerComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
