4d398ac89ca6a7d352f71b2a3bc7e60d9bc52b71
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-11-10 10:38:41 +0100

    Enums

---
+++ src/app/enums/motherboard-size-standard-enum.ts
@@ -0,0 +1,9 @@
+export enum MotherboardSizeStandardEnum
+{
+    ATX	= 1,
+    MicroATX = 2,
+    MiniATX	= 3,
+    EATX = 4,
+    WTX	= 5,
+    FlexATX = 6,
+}
---
+++ src/app/enums/ram-socket-enum.ts
@@ -0,0 +1,9 @@
+export enum RamSocketEnum
+{
+    SDRAM = 1,
+    DDR1 = 1,
+    DDR2 = 2,
+    DDR3 = 3,
+    DDR4 = 4,
+    DDR5 = 5,
+}
