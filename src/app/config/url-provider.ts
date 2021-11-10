003e61f21dc378d042353a7afb69498ae77996a6
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-11-10 11:52:13 +0100

    Create url-provider.ts

---
+++ src/app/config/url-provider.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class UrlProvider {
+    public readonly MOTHERBOARD: string = 'https://localhost:44397/api/motherboard';
+    public readonly CPU: string = 'https://localhost:44397/api/cpu';
+    public readonly RAM: string = 'https://localhost:44397/api/ram';
+}
