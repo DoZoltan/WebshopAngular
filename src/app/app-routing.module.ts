22ad7f5a3ddbbda0f9e8b57cf96e1ced02d203d1
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-11-12 14:18:42 +0100

    Products and user routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { UserComponent } from './components/administration/user/user.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GridComponent } from './components/product/grid/grid.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: 'products', component: GridComponent },
+  { path: 'user', component: UserComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
