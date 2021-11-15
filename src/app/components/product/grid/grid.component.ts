90d215e7aca60b3f5ec7f2610e83d3a61adb4da4
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-11-15 11:01:00 +0100

    Grid header and mock data

--- src/app/components/product/grid/grid.component.ts
+++ src/app/components/product/grid/grid.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GridComponent implements OnInit {
 
+  columnDefs = [
+    { headerName: "Name", field: "ProductName" },
+    { headerName: "Brand", field: "Brand" },
+    { headerName: "Price", field: "SellPrice" }
+  ];
+
+  products = [
+    { ProductName: '1', Brand: 'asd', SellPrice: 100 },
+    { ProductName: '2', Brand: 'dsa', SellPrice: 110 },
+    { ProductName: '3', Brand: 'sda', SellPrice: 120 },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
