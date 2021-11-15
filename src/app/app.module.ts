1c8f47bf2c35ceb2561f8f5ca2c7f62087b522ff
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-11-15 11:00:09 +0100

    Installed ag-grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { AgGridModule } from 'ag-grid-angular';
 
 import { AppComponent } from './app.component';
 import { AdminComponent } from './components/administration/admin/admin.component';
@@ -11,8 +12,8 @@ import { RegistrationComponent } from './components/administration/registration/
 import { HeaderNavBarComponent } from './components/navigation/header-nav-bar/header-nav-bar.component';
 import { LeftNavBarComponent } from './components/navigation/left-nav-bar/left-nav-bar.component';
 import { GridComponent } from './components/product/grid/grid.component';
-import { DetailsPopupComponent } from './components/product/details-popup/details-popup.component';
 import { ComponentContainerComponent } from './components/component-container/component-container.component';
+import { DetailsComponent } from './components/product/details/details.component';
 
 @NgModule({
   declarations: [
@@ -24,10 +25,10 @@ import { ComponentContainerComponent } from './components/component-container/co
     HeaderNavBarComponent,
     LeftNavBarComponent,
     GridComponent,
-    DetailsPopupComponent,
     ComponentContainerComponent,
+    DetailsComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, AgGridModule.withComponents([])],
   providers: [],
   bootstrap: [AppComponent],
 })
