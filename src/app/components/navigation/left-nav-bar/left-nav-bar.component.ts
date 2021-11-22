49d112ec7d9902dbd0f158f9e29aa5a900b29a08
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-11-22 11:33:29 +0100

    Check active route

--- src/app/components/navigation/left-nav-bar/left-nav-bar.component.ts
+++ src/app/components/navigation/left-nav-bar/left-nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-left-nav-bar',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeftNavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
   }
 
+  isActive(routeName: string): boolean
+  {
+    return routeName == this.route.url;
+  }
 }
