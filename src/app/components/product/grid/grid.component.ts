9c5fc247d4da33f6e92cbb4206da419c7aed4663
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-11-22 11:06:30 +0100

    Test methods to get actual products by route

--- src/app/components/product/grid/grid.component.ts
+++ src/app/components/product/grid/grid.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IBaseProductDTO } from 'src/app/interfaces/dto/ibase-product-dto';
 
 @Component({
   selector: 'app-grid',
@@ -13,15 +15,62 @@ export class GridComponent implements OnInit {
     { cellClass: 'grid-col', headerName: "Price", field: "SellPrice", resizable: true, sortable: true, filter: true}
   ];
 
-  products = [
-    { ProductName: '1', Brand: 'asd', SellPrice: 100 },
-    { ProductName: '2', Brand: 'dsa', SellPrice: 110 },
-    { ProductName: '3', Brand: 'sda', SellPrice: 120 },
+  products: IBaseProductDTO[] = [
+    { ProductName: 'asd', Brand: 'asd', SellPrice: 100 },
+    { ProductName: 'das', Brand: 'dsa', SellPrice: 110 },
+    { ProductName: 'sad', Brand: 'sda', SellPrice: 120 },
   ];
 
-  constructor() { }
+  constructor(private route: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
+    const currentRoute = this.route.url
+
+    switch (currentRoute) {
+      case '/products/cpu':
+        this.products = this.getCpus();
+        break;
+      case '/products/ram':
+        this.products = this.getRams();
+        break;
+      case '/products/motherboard':
+        this.products = this.getMotherboards();
+        break;
+    }
+  }
+
+  getCpus(): IBaseProductDTO[]
+  {
+    let Cpus: IBaseProductDTO[] = [
+      { ProductName: 'CPU 1', Brand: 'asd', SellPrice: 100 },
+      { ProductName: 'CPU 2', Brand: 'dsa', SellPrice: 110 },
+      { ProductName: 'CPU 3', Brand: 'sda', SellPrice: 120 },
+    ]
+    
+    return Cpus;
+  }
+
+  getRams(): IBaseProductDTO[]
+  {
+    let Cpus: IBaseProductDTO[] = [
+      { ProductName: 'RAM 1', Brand: 'asd', SellPrice: 100 },
+      { ProductName: 'RAM 2', Brand: 'dsa', SellPrice: 110 },
+      { ProductName: 'RAM 3', Brand: 'sda', SellPrice: 120 },
+    ]
+    
+    return Cpus;
+  }
+
+  getMotherboards(): IBaseProductDTO[]
+  {
+    let Cpus: IBaseProductDTO[] = [
+      { ProductName: 'Moth 1', Brand: 'asd', SellPrice: 100 },
+      { ProductName: 'Moth 2', Brand: 'dsa', SellPrice: 110 },
+      { ProductName: 'Moth 3', Brand: 'sda', SellPrice: 120 },
+    ]
+    
+    return Cpus;
   }
 
 }
