52b556c79851528b686cb9f62283236a04979dcb
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-11-22 17:50:30 +0100

    Get details from API

--- src/app/components/product/details/details.component.ts
+++ src/app/components/product/details/details.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { IMotherboard } from './../../../interfaces/products/imotherboard';
+import { IRam } from './../../../interfaces/products/iram';
+import { ICpu } from './../../../interfaces/products/icpu';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-details',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent implements OnInit {
+  product: any;
+  isProductACpu: boolean = false;
+  isProductARam: boolean = false;
+  isProductAMotherboard: boolean = false;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void{
   }
 
+  setDetails(product: ICpu | IRam | IMotherboard)
+  {
+    if ('coreNumber' in product) 
+    {
+      this.isProductACpu = true;
+    }
+    else if ('numberOfMemorySockets' in product)
+    {
+      this.isProductAMotherboard = true;
+    }
+    else if ('delay' in product)
+    {
+      this.isProductARam = true;
+    }
+
+    this.product = product;
+    console.log('this  ', product);
+  }
 }
