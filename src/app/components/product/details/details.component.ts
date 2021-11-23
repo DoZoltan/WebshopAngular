21101f0c01b6fc416b21a7d5e9e3502c73aa3155
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-11-23 08:47:25 +0100

    Show enum names

--- src/app/components/product/details/details.component.ts
+++ src/app/components/product/details/details.component.ts
@@ -2,6 +2,9 @@ import { IMotherboard } from './../../../interfaces/products/imotherboard';
 import { IRam } from './../../../interfaces/products/iram';
 import { ICpu } from './../../../interfaces/products/icpu';
 import { Component, Input, OnInit } from '@angular/core';
+import { CpuSocketEnum } from 'src/app/enums/cpu-socket-enum';
+import { RamSocketEnum } from 'src/app/enums/ram-socket-enum';
+import { MotherboardSizeStandardEnum } from 'src/app/enums/motherboard-size-standard-enum';
 
 @Component({
   selector: 'app-details',
@@ -33,8 +36,22 @@ export class DetailsComponent implements OnInit {
     {
       this.isProductARam = true;
     }
-
+    
     this.product = product;
-    console.log('this  ', product);
+  }
+
+  getRamSocketType(typeNumber: number): string
+  {
+    return RamSocketEnum[typeNumber];
+  }
+
+  getCpuSocketType(typeNumber: number): string
+  {
+    return CpuSocketEnum[typeNumber];
+  }
+
+  getMotherboardSizeStandardType(typeNumber: number): string
+  {
+    return MotherboardSizeStandardEnum[typeNumber];
   }
 }
