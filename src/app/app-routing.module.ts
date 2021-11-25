8b0a938565ce711d3b2a0c266e465b91a752460d
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-11-25 13:15:26 +0100

    Admin route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { UserComponent } from './components/administration/user/user.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GridComponent } from './components/product/grid/grid.component';
+import { AdminComponent } from './components/administration/admin/admin.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/products/cpu', pathMatch: 'full' },
   { path: 'products/:productType', component: GridComponent },
   { path: 'user', component: UserComponent },
   { path: 'login', component: AuthenticationComponent },
-  { path: 'registration', component: RegistrationComponent }
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'admin', component: AdminComponent }
 ];
 
 @NgModule({
