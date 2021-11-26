99d1404e34e3ffa5b3edc091e9f688410ed776f2
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-11-26 15:52:53 +0100

    Update component template and search functionality

--- src/app/components/product/update/update.component.ts
+++ src/app/components/product/update/update.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductTypeEnum } from 'src/app/enums/product-type-enum';
+import { IBaseProduct } from 'src/app/interfaces/products/ibase-product';
+import { CpuService } from 'src/app/services/product/cpu.service';
+import { MotherboardService } from 'src/app/services/product/motherboard.service';
+import { RamService } from 'src/app/services/product/ram.service';
+import { SearchService } from 'src/app/services/utility/search.service';
 
 @Component({
   selector: 'app-update',
@@ -6,10 +12,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update.component.scss']
 })
 export class UpdateComponent implements OnInit {
+  products: IBaseProduct[];
+  selectedProduct: any;
+  
+  columnDefs = [
+    { cellClass: 'grid-col', headerName: "Name", field: "productName", resizable: true, sortable: true, filter: true},
+  ];
 
-  constructor() { }
+  constructor(private searchService: SearchService,
+    private cpuService: CpuService, 
+    private ramService: RamService, 
+    private motherboardService: MotherboardService) { }
 
   ngOnInit(): void {
   }
 
+  getSearchResult(event: any)
+  {
+    if (this.isInputFieldNotEmpty(event.target.value))
+    {
+      this.searchService.SearchByProductName(event.target.value).subscribe(result => this.products = result);
+    }
+  }
+
+  private isInputFieldNotEmpty(inputContent: string): boolean
+  {
+    return (inputContent.length > 0 && !inputContent.startsWith(" "));
+  }
+
+  getProductDetailsById(event: any) 
+  {
+    switch (event.data.productType) {
+      case ProductTypeEnum.Cpu:
+        this.cpuService.getById(event.data.id).subscribe(product => this.selectedProduct = product);
+        break;
+      case ProductTypeEnum.Ram:
+        this.ramService.getById(event.data.id).subscribe(product => this.selectedProduct = product);
+        break;
+      case ProductTypeEnum.Motherboard:
+        this.motherboardService.getById(event.data.id).subscribe(product => this.selectedProduct = product);
+        break;
+    }
+  }
 }
