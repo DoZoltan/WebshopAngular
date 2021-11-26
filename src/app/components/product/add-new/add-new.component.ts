d421989c679893239775b176d6db4405a05fb97e
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-11-26 09:17:58 +0100

    Add new product menubar

--- src/app/components/product/add-new/add-new.component.ts
+++ src/app/components/product/add-new/add-new.component.ts
@@ -6,10 +6,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-new.component.scss']
 })
 export class AddNewComponent implements OnInit {
+  activeMenubarElement = {
+    cpu: true,
+    ram: false,
+    motherboard: false
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  isActive(menubarElementName: string): boolean
+  {
+    switch (menubarElementName) {
+      case 'cpu':
+        return this.activeMenubarElement.cpu;
+      case 'ram':
+        return this.activeMenubarElement.ram;
+      case 'motherboard':
+        return this.activeMenubarElement.motherboard;
+      default:
+        return false;
+    }
+  }
+
+  setActiveMenubarElement(menubarElementName: string)
+  {
+    switch (menubarElementName) {
+      case 'cpu':
+        this.activeMenubarElement.cpu = true;
+        this.activeMenubarElement.ram = false;
+        this.activeMenubarElement.motherboard = false;
+        break;
+      case 'ram':
+        this.activeMenubarElement.cpu = false;
+        this.activeMenubarElement.ram = true;
+        this.activeMenubarElement.motherboard = false;
+        break;
+      case 'motherboard':
+        this.activeMenubarElement.cpu = false;
+        this.activeMenubarElement.ram = false;
+        this.activeMenubarElement.motherboard = true;
+        break;
+      default:
+        this.activeMenubarElement.cpu = false;
+        this.activeMenubarElement.ram = false;
+        this.activeMenubarElement.motherboard = false;
+    }
+  }
 }
