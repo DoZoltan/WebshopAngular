a85ab2fb82f2c14f921a4f097b408ddc24305041
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-11-26 08:16:28 +0100

    Apply nested route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GridComponent } from './components/product/grid/grid.component';
 import { AdminComponent } from './components/administration/admin/admin.component';
+import { AddNewComponent } from './components/product/add-new/add-new.component';
+import { UpdateComponent } from './components/product/update/update.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/products/cpu', pathMatch: 'full' },
@@ -12,10 +14,13 @@ const routes: Routes = [
   { path: 'user', component: UserComponent },
   { path: 'login', component: AuthenticationComponent },
   { path: 'registration', component: RegistrationComponent },
-  { path: 'admin', component: AdminComponent },
-  { path: 'admin/users', component: AdminComponent },
-  { path: 'admin/products/add', component: AdminComponent },
-  { path: 'admin/products/update', component: AdminComponent }
+  { path: 'admin', component: AdminComponent,
+    children: [
+      { path: 'users', component: UserComponent },
+      { path: 'products/add', component: AddNewComponent },
+      { path: 'products/update', component: UpdateComponent }     
+    ] 
+  },
 ];
 
 @NgModule({
