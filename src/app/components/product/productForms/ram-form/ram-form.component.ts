42941a43ba9f406d8e88de8bf348fdb8b7d1ec3e
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-12-09 15:35:52 +0100

    Ram form grup

--- src/app/components/product/productForms/ram-form/ram-form.component.ts
+++ src/app/components/product/productForms/ram-form/ram-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ram-form',
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ram-form.component.scss']
 })
 export class RamFormComponent implements OnInit {
+  @Input() public ramToUpdate: any = null;
 
-  constructor() { }
+  ramForm = this.formBuilder.group(
+    {
+      productName: ['', [Validators.required]],
+      brand: ['', [Validators.required]],
+      imgURL: [''],
+      acquisitionPrice: ['', [Validators.required]],
+      sellPrice: ['', [Validators.required]],
+      productType: ['', [Validators.required]],
+      gb: ['', [Validators.required]],
+      delay: ['', [Validators.required]],
+      speed: ['', [Validators.required]],
+      socketType: ['', [Validators.required]],
+    }
+  );
 
-  ngOnInit(): void {
+  constructor(public formBuilder: FormBuilder) { }
+
+  ngOnInit(): void 
+  {
+
   }
 
+  ngOnChanges()
+  {
+    this.ramForm = this.formBuilder.group(
+      {
+        productName: [this.ramToUpdate?.productName, [Validators.required]],
+        brand: [this.ramToUpdate?.brand, [Validators.required]],
+        imgURL: [this.ramToUpdate?.imgURL],
+        acquisitionPrice: [this.ramToUpdate?.acquisitionPrice, [Validators.required]],
+        sellPrice: [this.ramToUpdate?.sellPrice, [Validators.required]],
+        productType: [this.ramToUpdate?.productType, [Validators.required]],
+        gb: [this.ramToUpdate?.gb, [Validators.required]],
+        delay: [this.ramToUpdate?.delay, [Validators.required]],
+        speed: [this.ramToUpdate?.speed, [Validators.required]],
+        socketType: [this.ramToUpdate?.socketType, [Validators.required]],
+      }
+    );
+  }
 }
