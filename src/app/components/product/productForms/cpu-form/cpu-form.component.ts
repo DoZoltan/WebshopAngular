1f51437f5cf5eef97b68e7e216e49939195a1fb0
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-12-10 07:48:00 +0100

    Apply reactive forms

--- src/app/components/product/productForms/cpu-form/cpu-form.component.ts
+++ src/app/components/product/productForms/cpu-form/cpu-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cpu-form',
@@ -6,10 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cpu-form.component.scss']
 })
 export class CpuFormComponent implements OnInit {
+  @Input() public cpuToUpdate: any = null;
 
-  constructor() { }
+  cpuForm = this.formBuilder.group(
+    {
+      productName: ['', [Validators.required]],
+      brand: ['', [Validators.required]],
+      imgURL: [''],
+      acquisitionPrice: ['', [Validators.required]],
+      sellPrice: ['', [Validators.required]],
+      productType: ['', [Validators.required]],
+      coreNumber: ['', [Validators.required]],
+      l3Cache: ['', [Validators.required]],
+      speed: ['', [Validators.required]],
+      socketType: ['', [Validators.required]],
+    }
+  );
+
+  constructor(public formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges()
+  {
+    this.cpuForm = this.formBuilder.group(
+      {
+        productName: [this.cpuToUpdate?.productName, [Validators.required]],
+        brand: [this.cpuToUpdate?.brand, [Validators.required]],
+        imgURL: [this.cpuToUpdate?.imgURL],
+        acquisitionPrice: [this.cpuToUpdate?.acquisitionPrice, [Validators.required]],
+        sellPrice: [this.cpuToUpdate?.sellPrice, [Validators.required]],
+        productType: [this.cpuToUpdate?.productType, [Validators.required]],
+        coreNumber: [this.cpuToUpdate?.coreNumber, [Validators.required]],
+        l3Cache: [this.cpuToUpdate?.l3Cache, [Validators.required]],
+        speed: [this.cpuToUpdate?.speed, [Validators.required]],
+        socketType: [this.cpuToUpdate?.socketType, [Validators.required]],
+      }
+    );   
   }
 
 }
--- src/app/components/product/productForms/motherboard-form/motherboard-form.component.ts
+++ src/app/components/product/productForms/motherboard-form/motherboard-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-motherboard-form',
@@ -6,10 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./motherboard-form.component.scss']
 })
 export class MotherboardFormComponent implements OnInit {
+  @Input() public motherboardToUpdate: any = null;
 
-  constructor() { }
+  motherboardForm = this.formBuilder.group(
+    {
+      productName: ['', [Validators.required]],
+      brand: ['', [Validators.required]],
+      imgURL: [''],
+      acquisitionPrice: ['', [Validators.required]],
+      sellPrice: ['', [Validators.required]],
+      productType: ['', [Validators.required]],
+      usb3Amount: ['', [Validators.required]],
+      wifi: ['', [Validators.required]],
+      sizeStandard: ['', [Validators.required]],
+      cpuSocketType: ['', [Validators.required]],
+      memorySocketType: ['', [Validators.required]],
+      maxMemorySize: ['', [Validators.required]],
+      numberOfMemorySockets: ['', [Validators.required]],
+    }
+  );
+
+  constructor(public formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  ngOnChanges()
+  {
+    this.motherboardForm = this.formBuilder.group(
+      {
+        productName: [this.motherboardToUpdate?.productName, [Validators.required]],
+        brand: [this.motherboardToUpdate?.brand, [Validators.required]],
+        imgURL: [this.motherboardToUpdate?.imgURL],
+        acquisitionPrice: [this.motherboardToUpdate?.acquisitionPrice, [Validators.required]],
+        sellPrice: [this.motherboardToUpdate?.sellPrice, [Validators.required]],
+        productType: [this.motherboardToUpdate?.productType, [Validators.required]],
+        usb3Amount: [this.motherboardToUpdate?.usb3Amount, [Validators.required]],
+        wifi: [this.motherboardToUpdate?.wifi, [Validators.required]],
+        sizeStandard: [this.motherboardToUpdate?.sizeStandard, [Validators.required]],
+        cpuSocketType: [this.motherboardToUpdate?.cpuSocketType, [Validators.required]],
+        memorySocketType: [this.motherboardToUpdate?.memorySocketType, [Validators.required]],
+        maxMemorySize: [this.motherboardToUpdate?.maxMemorySize, [Validators.required]],
+        numberOfMemorySockets: [this.motherboardToUpdate?.numberOfMemorySockets, [Validators.required]],
+      }
+    );
+  }
 }
